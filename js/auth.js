19f63ebb2918d28cd6d1b128296a312f002ffef9
Filipe Calegario <fcac@cin.ufpe.br>
2021-09-06 15:10:50 -0300

    Using auth instead of login

---
+++ js/auth.js
@@ -0,0 +1,17 @@
+const API_URL = 'https://api.strateegia.digital/users/v1/auth/signin';
+
+async function auth(username, password) {
+    const base64Login = btoa(`${username}:${password}`);
+
+    const response = await fetch(API_URL, {
+        method: 'post',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Basic ${base64Login}`
+        }
+    });
+
+    const data = await response.json();
+
+    return data.access_token;
+}
